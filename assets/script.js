ce31ea37bd973efa399800b26332b5aae127bb5b
Karla Fuentes <karla@iMac.local>
2023-02-19 23:17:26 +0000

    final changes for js file

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,13 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for (var x = 0; x <topics.length; x++) {
+function listTopics() {
+ for (var x = 0; x <topics.length; x++) {
     console.log(topics[x]);
+ }
 }
 
-/*
-if (topic === 'HTML') {
+function selectTopic() {
+ if (topic === 'HTML') {
  console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
  console.log("Let's study CSS!");  
@@ -14,6 +16,6 @@ if (topic === 'HTML') {
 } else if (topic === 'JavaScript') {
  console.log("Let's study JavaScript!");
 } else {
- console.log('Please try again!');
+  console.log('Please try again!');
+ }
 }
-*/
